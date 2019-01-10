f52b9b55ea72af64eca723bfee9dd73ea25dd61e
serhii.borovyk <serhii.borovyk@globallogic.com>
2019-01-10 18:13:22 +0200

    add Project entity with validation

---
+++ src/main/java/com/example/ppmtool/domain/Project.java
@@ -0,0 +1,46 @@
+package com.example.ppmtool.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Project name is required")
+    private String projectName;
+
+    @NotBlank(message = "Project identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
+    private String projectIdentifier;
+
+    @NotBlank(message = "Project description is required")
+    private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private LocalDateTime startDate;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private LocalDateTime endDate;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
